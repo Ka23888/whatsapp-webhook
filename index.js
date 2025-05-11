5a10d3fec51b7e43e5a47a677a9c6296ce9cafda
Ka23888 <126286773+Ka23888@users.noreply.github.com>
2025-05-11 10:55:27 -0400

    index.js

--- index.js
+++ index.js
@@ -8,19 +8,17 @@ app.get('/webhook', (req, res) => {
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
+
   if (mode === 'subscribe' && token === VERIFY_TOKEN) {
     return res.status(200).send(challenge);
   }
+
   res.sendStatus(403);
 });
 
 app.post('/webhook', (req, res) => {
-  const msg = req.body.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
-  if (msg) {
-    console.log("From:", msg.from);
-    console.log("Text:", msg.text?.body);
-  }
+  console.log('Webhook received:', req.body);
   res.sendStatus(200);
 });
 
-app.listen(3000, () => console.log("Running on 3000"));
+app.listen(3000, () => console.log('Running on 3000'));
